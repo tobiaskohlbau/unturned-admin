10a1ffe50707dd8817d812e749af0d99309dd9f2
Tobias Kohlbau <tobias@kohlbau.de>
2020-12-17 06:09:22 +0100

    add option to update the map without refreshing the page
    Signed-off-by: Tobias Kohlbau <tobias@kohlbau.de>

--- web/src/components/UnturnedMap.vue
+++ web/src/components/UnturnedMap.vue
@@ -1,10 +1,15 @@
 <template>
-  <s-card width="600px" height="650px">
+  <s-card width="600px" height="700px">
     <s-card-title>Map</s-card-title>
-    <div style="display: none">
-      <img src="/map.png" ref="map" />
-    </div>
-    <canvas ref="canvas" :style="{width: size + 'px', height: size + 'px'}"></canvas>
+    <s-card-content>
+      <div style="display: none">
+        <img src="/map.png" ref="map" />
+      </div>
+      <canvas ref="canvas" :style="{width: size + 'px', height: size + 'px'}"></canvas>
+    </s-card-content>
+    <s-card-actions>
+      <s-button @click.stop="updatePlayers">Update</s-button>
+    </s-card-actions>
   </s-card>
 </template>
 
@@ -32,48 +37,54 @@ export default defineComponent({
     const canvas: Ref<HTMLCanvasElement> = ref(null);
     const map: Ref<HTMLImageElement> = ref(null);
 
-    onMounted(async () => {
+    const updatePlayers = async () => {
+      const canvasCtx = canvas.value.getContext("2d");
+      canvasCtx.clearRect(0, 0, props.size, props.size);
+      canvasCtx.drawImage(map.value, 0, 0, props.size, props.size);
+
       const resp = await fetch("/api/players");
       const players = await resp.json();
+      players.forEach((item) => {
+        console.log(item);
+        const positionX = props.size * item.x;
+        const positionY = props.size * item.y;
+
+        const edge = 10;
+        var offsetX = 10;
+        if (positionX > (props.size - edge)) {
+          offsetX -= 12 * item.id.playerName.length;
+        }
+
+        var offsetY = 5;
+        if (positionY < edge) {
+          offsetY = 10;
+        }
+        if (positionY > (props.size - edge)) {
+          offsetY = -5;
+        }
+
+        canvasCtx.font = "12px Arial";
+        canvasCtx.fillStyle = "black";
+        canvasCtx.fillText(item.id.playerName, positionX + offsetX, positionY + offsetY);
+        canvasCtx.beginPath();
+        canvasCtx.fillStyle = "red";
+        canvasCtx.arc(positionX, positionY, 5, 0, 2 * Math.PI);
+        canvasCtx.fill();
+        canvasCtx.stroke();
+      });
+    };
+
+    onMounted(async () => {
       canvas.value.width = props.size;
       canvas.value.height = props.size;
-      const ctx = canvas.value.getContext("2d");
-      map.value.addEventListener("load", (e) => {
-        ctx.drawImage(map.value, 0, 0, props.size, props.size);
-        players.forEach((item) => {
-          const positionX = props.size * item.x;
-          const positionY = props.size * item.y;
-
-          const edge = 10;
-          var offsetX = 10;
-          if (positionX > (props.size - edge)) {
-            offsetX -= 12 * item.id.playerName.length;
-          }
-
-          var offsetY = 5;
-          if (positionY < edge) {
-            offsetY = 10;
-          }
-          if (positionY > (props.size - edge)) {
-            offsetY = -5;
-          }
-
-          ctx.font = "12px Arial";
-          ctx.fillStyle = "black";
-          ctx.fillText(item.id.playerName, positionX + offsetX, positionY + offsetY);
-          ctx.beginPath();
-          ctx.fillStyle = "red";
-          ctx.arc(positionX, positionY, 5, 0, 2 * Math.PI);
-          ctx.fill();
-          ctx.stroke();
-          ctx.save();
-        });
-      });
+      await new Promise(r => map.value.onload=r);
+      updatePlayers();
     });
 
     return {
       canvas,
       map,
+      updatePlayers,
     };
   },
 });
